2374f942cacbc8e17ef3ff02703f040142266466
wlaub <goldcray@gmail.com>
2016-04-28 09:06:25 -0700

    Added map for positions within rooms

---
+++ gmap.py
@@ -0,0 +1,68 @@
+import game
+
+class Map():
+    #Up right down left
+    locs = {
+    }
+
+    strings = {
+    "fail": "You can't go any farther {0}",
+    "pass": "You walk to the {0}. "
+    }
+
+    def go(self, _from, _dir):
+        _next = None
+        if _dir in self.locs[_from].keys():
+            _next = self.locs[_from][_dir]
+        if _next == None:
+            game.say(self.strings['fail'].format(_dir)) 
+            return None
+        game.sayBit(self.strings['pass'].format(_dir))
+        return _next
+
+
+class HalfMap(Map):
+    #01
+    locs = {
+    "left": {"right":"right"},
+    "right": {"left":"left"} 
+    }    
+
+
+class SquareMap(Map):
+    #01
+    #23
+    locs = {
+    "upper left": {"right": "upper right", "down": "lower left"},
+    "upper right": {"left": "upper left", "down": "lower right"},
+    "lower left": {"right": "lower right", "up": "upper left"},
+    "lower right": {"left": "lower left", "up": "upper right"},
+    }
+
+class GridMap(Map):
+    #012
+    #345
+    #678
+    locs = {
+    }
+
+class HorMap(Map):
+    #012
+    locs = {
+    }
+
+class VertMap(Map):
+    #1
+    #2
+    #3
+    locs = {
+    }
+
+class PlusMap(Map):
+    # 0
+    #123
+    # 4
+    locs = {
+
+    }
+
